fb6ad521b2449b5f370fd05bf6fb0b2ea3e65a69
laura-dancoso <lauradancoso@gmail.com>
2023-12-05 01:10:23 -0300

    update env

--- PFDancoso/src/environments/environment.prod.ts
+++ PFDancoso/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://json-server-api-angular-coderhouse.onrender.com/',
+  baseUrl: 'https://json-server-api-angular-coderhouse.onrender.com',
 };
